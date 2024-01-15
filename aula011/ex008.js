0fff05a26bf881fa2e29203eef4f79288378e87a
Ana Vaz <109620453+ana-vazz@users.noreply.github.com>
2024-01-15 18:07:17 +0000

    Aula 10 + Aula 11

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 80
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 50){ //condição simples
+    console.log(`Ultrapassou a velocidade permitida.`)
+}
+console.log(`Dirija sempre com segurança!`)
---
+++ aula011/ex009.js
@@ -0,0 +1,7 @@
+var pais = `Portugal`
+console.log(` Natural de ${pais}`)
+if (pais == `Portugal`){
+    console.log(`Português`)
+} else{
+    console.log(`Estrangeiro`)
+}
